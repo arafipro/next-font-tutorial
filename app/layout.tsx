51f4ad302031caace6ccc2d3df74076f8fcf1195
Hiroki Chiyoda <contact@arafipro.com>
2024-09-24 08:45:39 +0900

    feat: font setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const noto_Sans_JP = Noto_Sans_JP({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-noto",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={`${noto_Sans_JP.className} ${inter.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
